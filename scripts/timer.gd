fef2b7ce722835056640b3e1069d0106cec9393c
Medcro <muhammaddhiyarafi@gmail.com>
2025-11-18 20:56:45 +0700

    docs: add comments for timer.gd

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,12 +1,14 @@
-extends  Node
+extends Node
 class_name timer
 
+# getting nodes from timer nodes
 @onready var label = $Label
 @onready var timerCountdown = $Timer
 
 func _ready():
 	timerCountdown.start()
 
+# determined how many time left
 func time_left_to_finish():
 	var time_left = timerCountdown.time_left
 	var minute = floor(time_left / 60)
@@ -14,10 +16,11 @@ func time_left_to_finish():
 	return [minute, second]
 	
 func _process(delta):
-	label.text = "%02d : %02d" % time_left_to_finish()
+	# formatting time to look like -> 00:00
+	var format_string = "%02d : %02d"
+	label.text = format_string % time_left_to_finish()
 	timer_ended()
 	
 func timer_ended():
 	if time_left_to_finish() == [0.0, 0]:
 		print("You LOSE") # this is just a temporary placeholder
-	
